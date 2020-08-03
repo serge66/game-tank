7aae969cf256df8ace89adcfa7af0ffa75cf05b8
lsj <lishengjiejob@163.com>
2020-08-03 14:41:34 +0800

    ceshi

--- src/test/kotlin/TestWindow.kt
+++ src/test/kotlin/TestWindow.kt
@@ -1,2 +1,27 @@
-class TestWindow  {
+import javafx.application.Application
+import javafx.scene.input.KeyEvent
+import org.itheima.kotlin.game.core.Window
+
+class TestWindow : Window() {
+    override fun onCreate() {
+        println("  onCreate  ")
+    }
+
+    override fun onDisplay() {
+        println("  onDisplay  ")
+    }
+
+    override fun onKeyPressed(event: KeyEvent) {
+        println("  onKeyPressed  ")
+    }
+
+    override fun onRefresh() {
+        println("  onRefresh  ")
+    }
+
+
+}
+
+fun main(args:Array<String>) {
+    Application.launch(TestWindow::class.java)
 }
