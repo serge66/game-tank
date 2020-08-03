45e6cb323c219c9841bd1576bfb60fac5df4a724
lsj <lishengjiejob@163.com>
2020-08-03 15:01:22 +0800

    photo/voice

---
+++ src/main/kotlin/com/github/serge66/GameWindow.kt
@@ -0,0 +1,18 @@
+package com.github.serge66
+
+import javafx.scene.input.KeyEvent
+import org.itheima.kotlin.game.core.Window
+
+class GameWindow :Window() {
+    override fun onCreate() {
+    }
+
+    override fun onDisplay() {
+    }
+
+    override fun onKeyPressed(event: KeyEvent) {
+    }
+
+    override fun onRefresh() {
+    }
+}
